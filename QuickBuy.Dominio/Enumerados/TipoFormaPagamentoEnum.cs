1cca28e3af52b3c4abf234bd9900c422b6b31dd6
João Neves Aguilar <jaguilar@sinaf.com.br>
2022-06-17 15:00:07 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NãoDefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito = 3
+    }
+}
