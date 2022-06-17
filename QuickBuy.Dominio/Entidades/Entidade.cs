39f039c3282b8379f4ca9d74c21d943dd1249f0b
João Neves Aguilar <jaguilar@sinaf.com.br>
2022-06-17 15:26:29 -0300

    Implementando os validates

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,6 +1,30 @@
-﻿namespace QuickBuy.Dominio.Entidades
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagensValidacao { get; set; }
+        private List<string> mensagemValidacao
+        {
+            get { return _mensagensValidacao ?? new List<string>(); }
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
+
+        protected void LimparMensagemValidacao()
+        {
+            mensagemValidacao.Clear();
+        }
+
+        public abstract void Validate();
+        protected bool EhValido
+        {
+            get { return !MensagemValidacao.Any(); }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPeido { get; set; }
@@ -23,5 +24,15 @@ namespace QuickBuy.Dominio.Entidades
         /// Pedido deve ter pelo menos um item de pedido ou vários itens de pedidos
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+            if (!ItensPedido.Any())
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido");
+
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica - CEP deve estar preenchido");
+        }
     }
 }
